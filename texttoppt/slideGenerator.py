31775e635cc34b03ccb75de7341d9edc77ab04ef
Jatin Shrivastava <31873847+jatinsrivastava@users.noreply.github.com>
2019-06-27 21:11:18 +0530

    Update slideGenerator.py

--- texttoppt/slideGenerator.py
+++ texttoppt/slideGenerator.py
@@ -11,14 +11,12 @@ import codecs
 class SlideGenerator:
     def __init__(self, pptFileName ):
         self.pptFileName = pptFileName
-        with open('config.json') as config_file:
-            data = json.load(config_file)
-        self.shapeType = data['shapeType']
-        self.fontSize = int(data['fontSize'])
-        self.left = Inches(float(data['left']))
-        self.top = Inches(float(data['top']))
-        self.width = Inches(float(data['width']))
-        self.height = Inches(float(data['height']))
+        self.shapeType = 'round rectangle'
+        self.fontSize = int(28)
+        self.left = float(0.5)
+        self.top = float(0.40)
+        self.width = float(9.0)
+        self.height = float(6.60)
         self.shapemap = {}
         self.shapemap['rectangle'] = MSO_SHAPE.RECTANGLE
         self.shapemap['round rectangle'] = MSO_SHAPE.ROUNDED_RECTANGLE
@@ -27,10 +25,28 @@ class SlideGenerator:
         self.prs = Presentation()
         self.title_slide_layout = self.prs.slide_layouts[6]
 
+    def SetFontSize(self, size ):
+        self.fontSize = int(size)
+
+    def SetShapeType(self, shapeType ):
+        self.shapeType = shapeType
+
+    def SetShapeLeft(self, left ):
+        self.left = float(left)
+
+    def SetShapeTop(self, top ):
+        self.top = float(top)
+
+    def SetShapeWidth(self, width ):
+        self.width = float(width)
+
+    def SetShapeHeight(self, height ):
+        self.height = float(height)
+
     def addSlide(self, quote ):
         slide = self.prs.slides.add_slide(self.title_slide_layout)
         shapes = slide.shapes
-        shape = shapes.add_shape(self.shapemap[self.shapeType], self.left, self.top, self.width, self.height)
+        shape = shapes.add_shape(self.shapemap[self.shapeType], Inches(self.left), Inches(self.top), Inches(self.width), Inches(self.height))
         fill = shape.fill
         fill.solid()
         #fill.fore_color.rgb = RGBColor(0, 0, 0)
