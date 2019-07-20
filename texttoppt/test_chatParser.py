bcc7764fae125de54db9f59f6b584ddf658c7d37
Jatin Shrivastava <31873847+jatinsrivastava@users.noreply.github.com>
2019-07-20 20:29:44 +0530

    Update test_chatParser.py

--- texttoppt/test_chatParser.py
+++ texttoppt/test_chatParser.py
@@ -5,7 +5,7 @@ from chatParser import WhatsAppChatParser
 class TestWhatsAppChatParser(unittest.TestCase):
     def test_ignore_deleted_messages(self):
         test_data_file = "deletedLinesTestData.txt"
-        expected_output_text = "Quote 3\n"
+        expected_output_text = "Quote 3"
 
         chatparser = WhatsAppChatParser(test_data_file)
         chatparser.SetMessageAuthor("Aashish")
@@ -19,9 +19,9 @@ class TestWhatsAppChatParser(unittest.TestCase):
 
     def test_multiline_is_supported(self):
         test_data_file = "multilineTestData.txt"
-        expected_output_text = "Ignorance is bliss\n\n"
-        expected_output_text += "Its so painful to be aware of negative effects of my own actions\n\n"
-        expected_output_text += "Still falling victim of this uncontrolled mind\n"
+        expected_output_text = "Ignorance is bliss\n"
+        expected_output_text += "Its so painful to be aware of negative effects of my own actions\n"
+        expected_output_text += "Still falling victim of this uncontrolled mind"
         chatparser = WhatsAppChatParser(test_data_file)
         chatparser.SetMessageAuthor("Aashish")
         chatparser.ExtractQuoteList(test_data_file)
@@ -31,26 +31,35 @@ class TestWhatsAppChatParser(unittest.TestCase):
 
     def test_messages_on_or_after_a_date(self):
     	test_data_file = "dateFilterTestData.txt"
-    	message1 = "Quote4\n"
-    	message2 = "Quote5\n"
-    	chatparser = WhatsAppChatParser(test_data_file)
-    	chatparser.SetMessageAuthor("All")
-    	chatparser.SetStartDate("16/11/19")
-    	chatparser.ExtractQuoteList(test_data_file)
-    	self.assertEqual(chatparser.getNextQuote(), message1)
-    	self.assertEqual(chatparser.getNextQuote(), message2)
-
-    def test_messages_on_or_before_a_date(self):
-    	test_data_file = "dateFilterTestData.txt"
-    	message1 = "Quote3\n"
-    	message2 = "Quote4\n"
-    	message3 = "Quote5\n"
+    	message1 = "Quote 3"
+    	message2 = "Quote 4"
+    	message3 = "Quote 5"
     	chatparser = WhatsAppChatParser(test_data_file)
     	chatparser.SetMessageAuthor("All")
     	chatparser.SetStartDate("15/11/19")
-    	chatparser.SetEndDate("18/12/19")
     	chatparser.ExtractQuoteList(test_data_file)
     	self.assertEqual(chatparser.getNextQuote(), message1)
     	self.assertEqual(chatparser.getNextQuote(), message2)
     	self.assertEqual(chatparser.getNextQuote(), message3)
-    	#self.assertRaises(Exception,chatparser.getNextQuote())
+
+    def test_messages_on_or_before_a_date(self):
+    	test_data_file = "dateFilterTestData.txt"
+    	message1 = "Quote 1"
+    	message2 = "Quote 2"
+    	message3 = "Quote 3"
+    	chatparser = WhatsAppChatParser(test_data_file)
+    	chatparser.SetMessageAuthor("All")
+    	chatparser.SetEndDate("15/11/19")
+    	chatparser.ExtractQuoteList(test_data_file)
+    	self.assertEqual(chatparser.getNextQuote(), message1)
+    	self.assertEqual(chatparser.getNextQuote(), message2)
+    	self.assertEqual(chatparser.getNextQuote(), message3)
+
+    def test_capability_to_process_two_digits_date(self):
+    	pass
+
+    def test_capability_to_process_android_dateformat(self):
+    	pass
+
+    def test_capability_to_process_ios_dateformat(self):
+    	pass
